159196c17f84ac5bce3acfed771f3fdc8c972b25
artyomromaniv <romaniv.artyom8@gmail.com>
2023-05-15 18:29:27 +0300

    hw

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -14,78 +14,82 @@ import {useSearchParams} from 'react-router-dom'
 * */
 
 const getTechs = (find: string) => {
-    return axios
-       .get<{ techs: string[] }>(
-          'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test2',
-          {params: {find}}
-       )
-       .catch((e) => {
-           alert(e.response?.data?.errorText || e.message)
-       })
+   return axios
+      .get<{ techs: string[] }>(
+         'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test2',
+         {params: {find}}
+      )
+      .catch((e) => {
+         alert(e.response?.data?.errorText || e.message)
+      })
 }
 
-export const HW14 = () => {
-    const [find, setFind] = useState('')
-    const [isLoading, setLoading] = useState(false)
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [techs, setTechs] = useState<string[]>([])
+const HW14 = () => {
+   const [find, setFind] = useState('')
+   const [isLoading, setLoading] = useState(false)
+   const [searchParams, setSearchParams] = useSearchParams()
+   const [techs, setTechs] = useState<string[]>([])
 
-    const sendQuery = (value: string) => {
-        setLoading(true)
-        getTechs(value)
-           .then((res) => {
-               // делает студент
-               // сохранить пришедшие данные
-               //
-               if (res) {
-                   setLoading(false)
-                   setTechs(res.data.techs)
-               }
-           })
-    }
+   const sendQuery = (value: string) => {
+      setLoading(true)
+      getTechs(value)
+         .then((res) => {
+            // делает студент
+            if (res) {
+               setTechs(res.data.techs)
+               setLoading(false)
+            }
+            // сохранить пришедшие данные
 
-    const onChangeText = (value: string) => {
-        setFind(value)
-        // делает студент
+            //
+         })
+   }
 
-        // добавить/заменить значение в квери урла
-        // setSearchParams(
-        setSearchParams(value)
-        //
-    }
+   const onChangeText = (value: string) => {
+      setFind(value)
+      // делает студент
 
-    useEffect(() => {
-        const params = Object.fromEntries(searchParams)
-        sendQuery(params.find || '')
-        setFind(params.find || '')
-    }, [])
+      // добавить/заменить значение в квери урла
+      const newFind: {find?: string} = value ?  {find: value} : {}
+      const {find, ...restQueries} =  Object.fromEntries(searchParams)
+      setSearchParams({...newFind, ...restQueries})
+      // setSearchParams({find: value})
 
-    const mappedTechs = techs.map(t => (
-       <div key={t} id={'hw14-tech-' + t} className={s.tech}>
-           {t}
-       </div>
-    ))
+      //
+   }
 
-    return (
-       <div id={'hw14'}>
-           <div className={s2.hwTitle}>Homework #14</div>
+   useEffect(() => {
+      const params = Object.fromEntries(searchParams)
+      sendQuery(params.find || '')
+      setFind(params.find || '')
+   }, [])
 
-           <div className={s2.hw}>
-               <SuperDebouncedInput
-                  id={'hw14-super-debounced-input'}
-                  value={find}
-                  onChangeText={onChangeText}
-                  onDebouncedChange={sendQuery}
-               />
+   const mappedTechs = techs.map(t => (
+      <div key={t} id={'hw14-tech-' + t} className={s.tech}>
+         {t}
+      </div>
+   ))
 
-               <div id={'hw14-loading'} className={s.loading}>
-                   {isLoading ? '...ищем' : <br/>}
-               </div>
+   return (
+      <div id={'hw14'}>
+         <div className={s2.hwTitle}>Homework #14</div>
 
-               {mappedTechs}
-           </div>
-       </div>
-    )
+         <div className={s2.hw}>
+            <SuperDebouncedInput
+               id={'hw14-super-debounced-input'}
+               value={find}
+               onChangeText={onChangeText}
+               onDebouncedChange={sendQuery}
+            />
+
+            <div id={'hw14-loading'} className={s.loading}>
+               {isLoading ? '...ищем' : <br/>}
+            </div>
+
+            {mappedTechs}
+         </div>
+      </div>
+   )
 }
 
 export default HW14
